03891cc8f771a81b92f5cecfcf7d73790236b63b
BryceGuglietti <81780030+BryceGuglietti@users.noreply.github.com>
2022-07-19 19:53:50 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Quotes from './components/Quotes';
+function App() {
+  return (
+    <div className="App" id="quote-box">
+      <Quotes />
+    </div>
+  );
+}
+
+export default App;
